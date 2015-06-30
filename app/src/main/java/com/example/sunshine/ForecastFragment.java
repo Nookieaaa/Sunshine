d8b22bdfdcc4c7bbb9c97823ad5be33327e80ac7
Anton <nookieaaa@hotmail.com>
2015-06-30 18:02:04 +0300

    detail activity added

--- app/src/main/java/com/example/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.example.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -15,7 +16,6 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -68,7 +68,10 @@ public class ForecastFragment extends Fragment {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(getActivity(), mForecastAdapter.getItem(position).toString(), Toast.LENGTH_LONG).show();
+                //Toast.makeText(getActivity(), mForecastAdapter.getItem(position).toString(), Toast.LENGTH_LONG).show();
+                Intent detailIntent = new Intent(getActivity(),DetailActivity.class);
+                detailIntent.putExtra("forecast",mForecastAdapter.getItem(position));
+                startActivity(detailIntent);
             }
         });
 
